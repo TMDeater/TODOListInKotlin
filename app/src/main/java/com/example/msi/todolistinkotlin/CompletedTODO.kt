6ae3db3d4e9db934bb75e9370433b5769ee018ae
TMDeater <dannyng10231996@gmail.com>
2017-07-27 00:00:59 +0800

    Initial commit

---
+++ app/src/main/java/com/example/msi/todolistinkotlin/CompletedTODO.kt
@@ -0,0 +1,96 @@
+package com.example.msi.todolistinkotlin
+
+import android.content.Context
+import android.content.Intent
+import android.os.Environment
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.TextView
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileReader
+import java.io.FileWriter
+import java.io.IOException
+import java.util.ArrayList
+
+class CompletedTODO : AppCompatActivity() {
+    private var backBtn: TextView? = null
+    private var description: TextView? = null
+    private var completedTODO: ArrayList<String>? = null
+    private var arrAdapter: ArrayAdapter<String>? = null
+    private var lv: ListView? = null
+    private var context: Context? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_completed_todo)
+        title = "Completed TODOs"
+        context = this
+
+        backBtn = findViewById(R.id.go_back_button) as TextView
+        description = findViewById(R.id.description) as TextView
+        lv = findViewById(R.id.todolist) as ListView
+
+        backBtn!!.setOnClickListener { backPage() }
+
+        completedTODO = ArrayList<String>()
+
+        loadTODO()
+
+        arrAdapter = ArrayAdapter(context!!, android.R.layout.simple_list_item_1, completedTODO!!)
+
+        lv!!.adapter = arrAdapter
+        lv!!.onItemLongClickListener = AdapterView.OnItemLongClickListener { parent, view, position, id ->
+            RemoveTODO(position)
+            false
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun loadTODO() {
+        val path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM)
+        val file = File(path, "complete.txt")
+        if (!file.exists()){
+            file.createNewFile()
+        }
+        val br = BufferedReader(FileReader(file))
+        var line: String
+        while (br.readLine() != null) {
+            line = br.readLine()
+            completedTODO!!.add(line)
+        }
+        br.close()
+    }
+
+    @Throws(IOException::class)
+    private fun RemoveTODO(position: Int) {
+        completedTODO!!.removeAt(position)
+        val path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM)
+        val file = File(path, "complete.txt")
+        val writer = BufferedWriter(FileWriter(file, false))
+        for (todo in completedTODO!!) {
+            writer.write(todo)
+        }
+        writer.flush()
+        writer.close()
+        refresh()
+    }
+
+    private fun refresh() {
+        arrAdapter!!.clear();
+        arrAdapter!!.addAll(completedTODO);
+        arrAdapter!!.notifyDataSetChanged();
+    }
+
+    private fun backPage() {
+        val i = Intent()
+        i.setClass(this, MainActivity::class.java)
+        startActivity(i)
+    }
+}
---
+++ app/src/main/java/com/example/msi/todolistinkotlin/MainActivity.kt
@@ -0,0 +1,144 @@
+package com.example.msi.todolistinkotlin
+
+import android.content.Context
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.view.View
+import android.widget.*
+import java.io.*
+
+class MainActivity : AppCompatActivity() {
+    private var inputBox: EditText? = null
+    private var addItemBtn: TextView? = null
+    private var completeItemBtn: TextView? = null
+    private var tutorialText: TextView? = null
+    private var completedTODO: ArrayList<String>? = null
+    private var todoList: ArrayList<String>? = null
+    private var arrAdapter: ArrayAdapter<String>? = null
+    private var lv: ListView? = null
+    private var context: Context? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        title = "TODO list"
+
+        inputBox = findViewById(R.id.input_box) as EditText
+        addItemBtn = findViewById(R.id.add_item_button) as TextView
+        completeItemBtn = findViewById(R.id.complete_item_button) as TextView
+        tutorialText = findViewById(R.id.tutorial) as TextView
+        lv = findViewById(R.id.todolist) as ListView
+
+        (addItemBtn as TextView).setOnClickListener ( object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                addItem()
+            }
+        })
+        (completeItemBtn as TextView).setOnClickListener(object: View.OnClickListener{
+            override fun onClick(v: View?) {
+                ToCompletePage()
+            }
+        })
+
+        completedTODO = ArrayList<String>()
+        todoList = ArrayList<String>()
+
+        loadCompletedTODO()
+        loadTODO()
+
+        arrAdapter = ArrayAdapter(context!!, android.R.layout.simple_list_item_1, todoList!!)
+        
+        lv!!.adapter = arrAdapter
+        lv!!.onItemLongClickListener = AdapterView.OnItemLongClickListener { parent, view, position, id ->
+            RemoveTODO(position)
+            false
+        }
+        lv!!.onItemClickListener =AdapterView.OnItemClickListener { parent, view, position, id ->
+            completedTODO!!.add(todoList!![position])
+            RemoveTODO(position)
+            val path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM)
+            val file = File(path, "complete.txt")
+            val writer = BufferedWriter(FileWriter(file, false))
+            for (todo in completedTODO!!) {
+                writer.write(todo)
+            }
+            writer.flush()
+            writer.close()
+        }
+    }
+
+
+    @Throws(IOException::class)
+    private fun loadTODO() {
+        val path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM)
+        val file = File(path, "todolist.txt")
+        if (!file.exists()){
+            file.createNewFile()
+        }
+        val br = BufferedReader(FileReader(file))
+        var line: String
+        while (br.readLine() != null) {
+            line = br.readLine()
+            todoList!!.add(line)
+        }
+        br.close()
+    }
+
+    @Throws(IOException::class)
+    private fun loadCompletedTODO() {
+        val path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM)
+        val file = File(path, "complete.txt")
+        if (!file.exists()){
+            file.createNewFile()
+        }
+        val br = BufferedReader(FileReader(file))
+        var line: String
+        while (br.readLine() != null) {
+            line = br.readLine()
+            completedTODO!!.add(line)
+        }
+        br.close()
+    }
+
+    @Throws(IOException::class)
+    private fun RemoveTODO(position: Int) {
+        todoList!!.removeAt(position)
+        val path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM)
+        val file = File(path, "todolist.txt")
+        val writer = BufferedWriter(FileWriter(file, false))
+        for (todo in todoList!!) {
+            writer.write(todo)
+        }
+        writer.flush()
+        writer.close()
+        refresh()
+    }
+
+    @Throws(IOException::class)
+    private fun addItem() {
+        todoList!!.add(inputBox!!.text.toString())
+        val path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM)
+        val file = File(path, "todolist.txt")
+        val writer = BufferedWriter(FileWriter(file, false))
+        for (todo in todoList!!) {
+            writer.write(todo)
+        }
+        writer.flush()
+        writer.close()
+        refresh()
+    }
+
+    private fun refresh() {
+        arrAdapter!!.clear();
+        arrAdapter!!.addAll(todoList);
+        arrAdapter!!.notifyDataSetChanged();
+    }
+
+    private fun ToCompletePage() {
+        val intent = Intent(this,CompletedTODO::class.java)
+        startActivity(intent)
+    }
+
+}
